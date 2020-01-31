6df109a0dde10022909d924dfa79ed1e3fd2bc14
Rodrigue-coder <edjuitcheukorod@yahoo.fr>
2020-01-31 10:40:27 +0100

    Add the rest of the code

---
+++ script.js
@@ -0,0 +1,60 @@
+/*The next line will show a popup message*/
+//alert('Hello world!')
+const FIRST_NAME = 'Edjui Tcheuko'
+const LAST_NAME = 'Rodrigue'
+
+let fullName = FIRST_NAME +' '+ LAST_NAME
+const myAddress = 'Via Assisi, 40\n10149 Torino (TO)\nItaly'
+
+
+const STREET_NAME = 'via Assisi'
+const HOUSE_NUMBER = '40'
+const ZIP_CODE = '10149'
+const CITY = 'Torino'
+const COUNTRY = 'Italy'
+const PROVINCE = 'TO'
+
+let Address = '(' + STREET_NAME + '\n' + HOUSE_NUMBER + ',' + ZIP_CODE + ' ' + CITY + ' ' + COUNTRY + ')'
+
+const Nom_Famille = 'Kamlo'
+const Nom_Famille_2 = 'Edjui'
+const Nom_Famille_3 = 'Tcheuko'
+const Prenom_1 = 'Kemi'
+const Prenom_2 = 'Koni'
+const Space = ' '
+const New_line = '\n'
+
+let myChild = Nom_Famille + ' ' + Nom_Famille_2 + ' ' + Nom_Famille_3 + ' ' + New_line + Prenom_1 + ' ' + Prenom_2 
+
+const newAddress = `${STREET_NAME} ${HOUSE_NUMBER} 
+${ZIP_CODE} 
+${CITY} (${PROVINCE})
+${COUNTRY}`
+
+const Work_Experience_1 = 'Sales Assistant BricoCenter'
+const Work_Experience_2 = 'Welder'
+const Work_Experience_3 = 'Translator'
+const Studies_1 = 'Master 2 in Communication Science'
+const Studies_2 = 'Master 1 in Communication'
+const Studies_3 = 'Master in investigation'
+const Skill_1 = 'Team Work'
+const Skill_2 = 'Communication abilities'
+const Skill_3 = 'Adaptability'
+const Speaking_Languages_1 = 'French'
+const Speaking_Languages_2 = 'English'
+const Speaking_Languages_3 = 'Italian'
+
+let cv = `${Work_Experience_1} 
+${Work_Experience_2}
+${Work_Experience_3} 
+${Studies_1} 
+${Studies_2} 
+${Studies_3} 
+${Skill_1} 
+${Skill_2} 
+${Skill_3} 
+${Speaking_Languages_1} 
+${Speaking_Languages_2} 
+${Speaking_Languages_3}`
+
+console.log(cv)
