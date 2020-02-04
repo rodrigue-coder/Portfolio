f27b508588332d672c7d24f48557159421d62985
Rodrigue-coder <edjuitcheukorod@yahoo.fr>
2020-02-04 17:45:49 +0100

    add exo.js + loops.js

---
+++ exo.js
@@ -0,0 +1,22 @@
+//let i = 3;
+
+//while (i) {
+  //console.log( i-- );
+//}
+//console.log(i);
+
+//let i = 0;
+//while (i++ < 5){
+//}
+//console.log(i);
+
+//for (let i = 0; i < 5; ++i) console.log(i);
+
+//for (let i = 0; i < 5; i++) console.log( i );
+
+//Use the for loop to output even numbers from 2 to 10.
+//for (let i = 1; i < 5; ++i) console.log(i * 2);
+
+//for (let i = 2; i <= 10; i++) {    if (i % 2 == 0) {      alert( i );    }  }
+
+//let i = 0; while (i++ < 3) {  console.log(i); }
---
+++ loops.js
@@ -0,0 +1,145 @@
+// variable called i that was initialized as 0
+// while loop that tested i against 3
+// the loop body alerted i (console.log) and incremented i
+// the final result was: printing 0, 1, 2
+
+// declare a variable i whose value is 2
+// check if the number is greater than o
+// print the variable and decrement it
+
+// let i = 2; 
+// while (i > 0) {
+// console.log(i);
+// i--;
+// }
+
+/*
+// i want to built a string that looks like this:
+// 16 times 
+// "Na Na Na Na ....  Batman!"
+// The string should be builts by looping 16 times, and at the end of the loop I want to print the result to the console
+*/
+
+
+let str;
+let count = 0
+while (count < 16) {
+    console.log("Na");
+    count++;
+}
+console.log("Batman!");
+
+
+let str = "a"
+str += "b";
+str += "c";
+console.log("Batman")
+
+
+str = "a " + "b " + "c " + "Batman " + "!"
+console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
