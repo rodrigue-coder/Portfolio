bd226b47a3adcb0eb351876413c382b8fb435ec2
Rodrigue-coder <edjuitcheukorod@yahoo.fr>
2020-02-10 17:21:11 +0100

    add switch.js

---
+++ switch.js
@@ -0,0 +1,102 @@
+//let car = 'A'
+
+// switch (car) {
+//     case 'A':
+//         console.log('No')
+//     break
+
+//     case 'J':
+//     console.log('Yes')
+//     break
+
+//     case 'Q':
+//         console.log('Forse')
+//     break
+
+//     case 'K':
+//     console.log('Why not?')
+//     break
+
+   // default: 
+   // console.log('Really?')
+
+
+
+// }
+
+// let case = 'Browser'
+
+// switch (Browser) {
+//     case 'Firefox':
+//         alert("You have got Firefox")
+//         break;
+//         case
+// }
+
+// let browser = prompt('What browser do you have?'); // +"1" => 1
+
+// if (browser == 'Edge') {
+//     console.log("You've got the Edge!" );
+// } 
+// else if (browser == 'Chrome' ||) {
+//     console.log("You've got the Chrome");
+// }
+// else if (browser == 'Firefox') {
+//     console.log("You've got the Firefox");
+// }
+// else if  (browser == 'Opera') {
+//     console.log("You've got the Opera");
+// }
+// else if (browser == 'Safari') {
+//     console.log("You've got theSafari");
+// }
+// else {console.log("We hope that this page looks ok!");
+
+// }
+
+
+// let browser = prompt('What browser do you have?'); // +"1" => 1
+
+// if (browser == 'Edge' || browser == 'Firefox' || browser == 'Opera' || browser == 'Safari' || browser == 'Chrome') 
+// {
+//     console.log("You've got the ..." + browser);
+// }
+// else {
+//     console.log("We hope that this page looks ok!");
+
+// }
+
+// let browser = 0 ;
+
+// while  (true){
+//     browser = prompt('What browser do you have?'); 
+//      if (browser == 'Edge' || browser == 'Firefox' || browser == 'Opera' || browser == 'Safari' || browser == 'Chrome') 
+//      console.log("You've got the ..." + browser);
+//      else if (browser == 'stop') break; 
+//      else 
+//      {
+//         console.log("Riprova");
+//     }
+// }
+
+let a = +prompt('a?', '');
+
+switch (a) {
+    
+    case '0':
+    alert (0);
+  break; 
+
+    case '1':
+        alert (1);
+    break;
+
+    case '2':
+        alert (2);
+        break
+
+    case '3':
+        alert (3);
+        break;
+}
+
